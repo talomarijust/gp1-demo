4e0786326aac902e8e7b06ebad5572526c705699
talomarijust <talomari@just.edu.jo>
2025-11-09 00:43:22 +0300

    changes to pytest

--- test_app.py
+++ test_app.py
@@ -1,12 +1,8 @@
-import unittest
 from app import add_numbers
 
-class TestMath(unittest.TestCase):
-    def test_addition_correct(self):
-        self.assertEqual(add_numbers(2, 3), 5)  # ✅ Pass
+def test_addition_correct():
+    assert add_numbers(2, 3) == 5
 
-    def test_addition_fail(self):
-        self.assertEqual(add_numbers(2, 3), 5)  # ❌ Fail (for demo)
-
-if __name__ == "__main__":
-    unittest.main()
+def test_addition_fail():
+    # This test will fail intentionally to demonstrate CI catching it
+    assert add_numbers(2, 3) == 5
